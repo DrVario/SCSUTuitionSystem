e2e688620ca3b52ce1f066d23ac1d6b0cd5efda2
Grant <gtporter@stcloudstate.edu>
2019-03-12 02:03:28 -0500

    Added bootstrap and first pass at the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, {
   Component
 } from 'react';
 
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+
 
 
 class App extends Component {
@@ -33,22 +35,33 @@ class App extends Component {
     }
     return(
       <div>
-          <div>ID: {this.state.Infromation.Id} </div>
-          <div>Semester: {this.state.Infromation.Semester}</div>
-          <div>Payment History Date Paid: {this.state.Infromation.PaymentHistory[0]}</div>
-          <div>Payment History Payment Amount: {this.state.Infromation.PaymentHistory[1]}</div>
-          <div>Payment History Payment Source: {this.state.Infromation.PaymentHistory[2]}</div>
-          <div>Tuition Breakdown Activity Fee: {this.state.Infromation.TuitionBreakdown[0]}</div>
-          <div>Tuition Breakdown MSUSA: {this.state.Infromation.TuitionBreakdown[1]}</div>
-          <div>Tuition Breakdown Technology Fee: {this.state.Infromation.TuitionBreakdown[2]}</div>
-          <div>Tuition Breakdown Athletics Fee: {this.state.Infromation.TuitionBreakdown[3]}</div>
-          <div>Tuition Breakdown Facilties Assesment: {this.state.Infromation.TuitionBreakdown[4]}</div>
-          <div>Tuition Breakdown Student Union: {this.state.Infromation.TuitionBreakdown[5]}</div>
-          <div>Tution Breakdown Health Services: {this.state.Infromation.TuitionBreakdown[6]}</div>
-          <div>View Scholarships Scholarship Amount: {this.state.Infromation.ViewScholarships[0]}</div>
-          <div>View Scholarships Scholarship Source: {this.state.Infromation.ViewScholarships[2]}</div>
-          <div>Student Employment Routing Number: {this.state.Infromation.StudentEmployment[0]}</div>
-          <div>Student Employment Hours Worked: {this.state.Infromation.StudentEmployment[1]}</div>
+        {/* Below is listed the custom css for the bootstrap module. For most custom things
+        relating to styles we should define them in the index.css file, but for special
+        things such as bootstrap we need to do them inline like below. */}
+          <style type="text/css">
+          {`
+          .bg-red {
+            background-color: #CD1041;
+          }
+          `}
+        </style>
+        {/* Here is the start of the website. This will be the navbar at the top of the site that will
+        render throught the whole site. This is based off of the traditional bootstrap navbar.
+        Something to notice is that all the assets such as the logo are coming from the assets file located at
+        client\src\assets. Anything that has to do with some sort of picture or outside asset needs to be stored
+        there. For more references as to what is happening here visit https://react-bootstrap.netlify.com/components/navbar */}
+      <Navbar bg="red" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={require('./assets/logo.svg')}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          {' St. Cloud State Tuition '}
+        </Navbar.Brand>
+      </Navbar>
       </div>
       );
     }
