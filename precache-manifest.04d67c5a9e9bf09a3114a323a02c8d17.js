7f6003a08c45c92a1c4fa30e79fcce4b26959b31
Grant <gtporter@stcloudstate.edu>
2019-04-26 01:46:59 -0500

    Updates

---
+++ precache-manifest.04d67c5a9e9bf09a3114a323a02c8d17.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2dde3acecdc361a5124",
+    "url": "/SCSUTuitionSystem/static/css/main.56e2858b.chunk.css"
+  },
+  {
+    "revision": "e2dde3acecdc361a5124",
+    "url": "/SCSUTuitionSystem/static/js/main.e2dde3ac.chunk.js"
+  },
+  {
+    "revision": "b0ff595abb5073ed4fee",
+    "url": "/SCSUTuitionSystem/static/js/runtime~main.b0ff595a.js"
+  },
+  {
+    "revision": "0c165a885488dc82c3e8",
+    "url": "/SCSUTuitionSystem/static/css/2.6f40247a.chunk.css"
+  },
+  {
+    "revision": "0c165a885488dc82c3e8",
+    "url": "/SCSUTuitionSystem/static/js/2.0c165a88.chunk.js"
+  },
+  {
+    "revision": "573c825ed6868cec78fa346c8791f052",
+    "url": "/SCSUTuitionSystem/static/media/logo.573c825e.svg"
+  },
+  {
+    "revision": "76dd097fad6cf0071645106874c0086d",
+    "url": "/SCSUTuitionSystem/static/media/group.76dd097f.png"
+  },
+  {
+    "revision": "4422ddd50ecb289a6cf60d9981b82b7c",
+    "url": "/SCSUTuitionSystem/static/media/river.4422ddd5.png"
+  },
+  {
+    "revision": "ce4bb0234b821baab32f29e145997879",
+    "url": "/SCSUTuitionSystem/static/media/bridge.ce4bb023.png"
+  },
+  {
+    "revision": "6d3a0eeac0404394da6f4932756009ce",
+    "url": "/SCSUTuitionSystem/static/media/bed.6d3a0eea.png"
+  },
+  {
+    "revision": "af307123545133fe0bb741e8ae5213cd",
+    "url": "/SCSUTuitionSystem/static/media/hockey.af307123.png"
+  },
+  {
+    "revision": "27c26b74832e1f944a03335f4f9a4d99",
+    "url": "/SCSUTuitionSystem/static/media/iself.27c26b74.png"
+  },
+  {
+    "revision": "8761c3edd2baa217d6cd6465998472ef",
+    "url": "/SCSUTuitionSystem/index.html"
+  }
+];
